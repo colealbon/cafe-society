8b974fdb39926c5fe889f9807286b9d47abfb567
colealbon <colealbon@gmail.com>
2019-08-31 18:22:48 -0700

    fiddle with gaiaLinks

--- src/actions/articleActions.js
+++ src/actions/articleActions.js
@@ -56,7 +56,7 @@ export const markArticleRead = (articles, allArticles, gaiaLinks) => {
       type: ARTICLES_MARK_READ,
       payload: articles
     })
-    dispatch(publishArticles(articles.map((stateArticle) => {
+    dispatch(publishArticles([].concat(articles).map((stateArticle) => {
       let articleMatched = false
       articles = [].concat(articles)
       articles.map((toggleArticle) => {
@@ -292,6 +292,7 @@ export const publishArticles = (articles, gaiaLinks) => {
             }
           })
         })
+      return '0'
       })
     })
   }
